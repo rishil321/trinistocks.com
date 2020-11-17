47e26ed89a075e8bf0cb1b904fea2bf985fde1c9
rishi321 <latchmepersad@gmail.com>
2020-11-17 14:34:14 -0400

    changed email domain
    Former-commit-id: ddb392bb3bcf7b55bc76e60ddce5a989a3ea0321 [formerly 99ff0d84dba92f42c5ee86c684de003f7fef4d04] [formerly 19523d98cddd524b9296ca26ab11462d28dcb405 [formerly 4915446dd22f98ff97ea6922ec5d7d9a51986dd7]]
    Former-commit-id: 308e568dacf0c6f93c66816a4a7c3ed45c8c8bc8 [formerly a823755faf910bf83e35e6792f99b65d6d31cbe2]
    Former-commit-id: 2890c1ec7fc36771deef496493faa2de543a83ea

--- trinistats/stocks/views.py
+++ trinistats/stocks/views.py
@@ -1248,12 +1248,12 @@ class PasswordResetRequestView(FormView):
             email_template_name = "stocks/account/password_reset_email.txt"
             email_body = {
             "email":user.email,
-            'domain':'127.0.0.1:8000',
+            'domain':'trinistats.com',
             'site_name': 'stocks',
             "uid": urlsafe_base64_encode(force_bytes(user.pk)),
             "user": user,
             'token': default_token_generator.make_token(user),
-            'protocol': 'http',
+            'protocol': 'https',
             }
             email = render_to_string(email_template_name, email_body)
             send_mail(
