4485d916503c7d181fb4bed2cb912943b89d0b24
rishi321 <latchmepersad@gmail.com>
2021-01-03 20:34:35 -0400

    initial commit
    Former-commit-id: f306cfa3d8ba789074c8d6510482341a622b7ec3 [formerly 367b34f32c4e583bd59341e1122103e0648dc6e5] [formerly f7ba65187875bf550c76cc9aa89baa279bb3eb42 [formerly cd7cddd6bfdb2f39360d7e04d0e49bdf0dbc6ad1]]
    Former-commit-id: d2fe115fc6e2c6ca4a274987575d85fd0c7e0be0 [formerly bbb0c2e34864fbba2712d12c6abe95141b69fa0c]
    Former-commit-id: 13228e04a67adb476bf654e64d9a416e43981d92

--- trinistats/covid19/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Covid19_Paho_Reports
-
-# Register your models here.
-admin.site.register(Covid19_Paho_Reports)
--- trinistats/scripts/covid19/trinistatsconfig.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
- 
-"""This Python script is used to store credentials that we want to .gitignore"""
-dbusername = "script"
-dbpassword = "scriptsecretpassword"
-dbaddress = "127.0.0.1"
-dbport = "3306"
-schema = "trinistatsdb"
