925031b48c4e0f71a4741a4fa4f0d79b8903436d
rishi321 <latchmepersad@gmail.com>
2021-01-06 11:19:05 -0400

    rere
    Former-commit-id: 62b7439e40555f812d5f1b47506da65724ff49b3 [formerly e54f4a90213fc86b6d6aac122f9b90d2cba1966f] [formerly 1586c915619cc64000a739d7d70e95d730a38fa1 [formerly 2c721643c375ef4150d29582b4557e8383685b49]]
    Former-commit-id: 1a1a91c0c6bd8be83d26ad78b1ebd630ef1ab665 [formerly 22e12ade5023bab9a2011b17d1fb8ec7ab5ee606]
    Former-commit-id: 229061965ae3d6f64025d9ca490235c8b134a992

--- trinistocks/trinistocks/settings.py
+++ trinistocks/trinistocks/settings.py
@@ -150,8 +150,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
+r
+STATIC_URL = '/static'
 STATIC_ROOT = os.path.join(
     BASE_DIR, '/srv/Github/trinistocks.com/trinistocks/static')
 
