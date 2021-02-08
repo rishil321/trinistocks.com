be948967d62c177a56857005350d32c8ccc64972
rishi321 <latchmepersad@gmail.com>
2021-02-08 14:53:33 -0400

    added news scraping
    Former-commit-id: c81343a8e5a849312a2929a9f8a6bcc7ab36bdc8 [formerly 046efcf1d6a80e07c0627036857f5d2acb09b468] [formerly f6757d61fd6b5b4c49e526fde9af01bacffd54b9 [formerly e8891fd39814678ceaaee8e25a573e140bd818ab]]
    Former-commit-id: e5a586effba8e2c633514ee377380ff441c42041 [formerly b17f017571718887085555379be5c8655f38a923]
    Former-commit-id: 0b0c6a0193c0d9328bbcda07a67647538e9f1c24

--- trinistocks/scripts/stocks/scrapettse/scraper.py
+++ trinistocks/scripts/stocks/scrapettse/scraper.py
@@ -49,7 +49,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # The timeout to set for multiprocessing tasks (in seconds)
 MULTIPROCESSING_TIMEOUT = 60*60
-WEBPAGE_LOAD_TIMEOUT_SECS = 30
+WEBPAGE_LOAD_TIMEOUT_SECS = 60
 # Define a start date to use for the full updates
 START_DATE = '2017-01-01'
 LOGGERNAME = 'scraper.py'
