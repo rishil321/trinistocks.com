9f9c941c0e992496816c0a26db1e858692c77fa7
Rishi Latchmepersad <latchmepersad@gmail.com>
2021-03-07 15:08:57 -0400

    news stuff
    Former-commit-id: 953bf64d4095fb4515cdbb9e4634ca2b1993b02d [formerly dc35754778f161b6026e525d1090d4e7bfb51765] [formerly 9799302d0c1a26bc341ed5ff0d6fa8a6946df101 [formerly 8d1fe750436aa8e73fb58e4a60571784b13b22fb]]
    Former-commit-id: bb5412da5681d8d3b911c20de3cffbd3f09fcc2f [formerly 80d616b417271268254ce762b0537bd8a24d32f3]
    Former-commit-id: cfb72e2995025c5f25ea873e317f2c8da3d11eae

--- trinistocks/scripts/stocks/scrapettse/old_test_fundamental_data_parser.py
+++ trinistocks/scripts/stocks/scrapettse/old_test_fundamental_data_parser.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
- 
+
 """
 The testing module for the scraper.py file
 :param -f OR --first_parameter: The description of your first input parameter
 :returns: Whatever your script returns when called
 :raises Exception if any issues are encountered
 """
- 
-# Put all your imports here, one per line. 
+
+# Put all your imports here, one per line.
 # However multiple imports from the same lib are allowed on a line.
 # Imports from Python standard libraries
 import sys
@@ -23,35 +23,40 @@ from . import fundamental_data_parser
 
 # Put your constants here. These should be named in CAPS.
 
-# Put your global variables here. 
- 
+# Put your global variables here.
+
 # Put your class definitions here. These should use the CapWords convention.
- 
+
 # Put your function definitions here. These should be lowercase, separated by underscores.
 def test_convert_quarterly_reports_text():
     assert fundamental_data_parser.convert_quarterly_reports_text() == 0
 
+
 def test_convert_annual_reports_text():
     assert fundamental_data_parser.convert_annual_reports_text() == 0
+
 
 def test_process_quarterly_reports():
     assert fundamental_data_parser.process_quarterly_reports() == 0
 
+
 def test_process_annual_reports():
     assert fundamental_data_parser.process_annual_reports() == 0
- 
+
+
 def main():
     """Docstring description for each function"""
     try:
         # All main code here
         pass
     except Exception:
-        logging.exception("Error in script "+os.path.basename(__file__))
+        logging.exception("Error in script " + os.path.basename(__file__))
         sys.exit(1)
     else:
-        logging.info(os.path.basename(__file__)+" executed successfully.")
+        logging.info(os.path.basename(__file__) + " executed successfully.")
         sys.exit(0)
- 
+
+
 # If this script is being run from the command-line, then run the main() function
 if __name__ == "__main__":
     main()
