3ac01085a4945a3dfcc9c89005f7dc03bb368185
Rishi Latchmepersad <latchmepersad@gmail.com>
2021-03-08 17:33:27 -0400

    reformatted forms
    Former-commit-id: f0b6fa3d2a69a3011f1820848ffa52ab1529e197 [formerly 3b53d0de4cbe0988c7aea246f9e5c3d9c2f8365c] [formerly 707ec0533d691f995ddb0d090b955bbdb0086bf0 [formerly ebce20c8633dd4436e8b1f89ac13144e1f78e428]]
    Former-commit-id: 08987adbe9f1b00837b9737a05107ad91478e17a [formerly 9d60f62765d130af0565f3cb0b93c0580cae8797]
    Former-commit-id: 5b5f6b8529ac621b26f1ac668772002e4d4ae3d5

--- trinistocks/stocks/forms.py
+++ trinistocks/stocks/forms.py
@@ -3,36 +3,42 @@ from django import forms
 from django.core.exceptions import ValidationError
 import django.core.validators as validators
 import django.contrib.auth.password_validation as password_validators
-from django.contrib.auth import authenticate,get_user_model
+from django.contrib.auth import authenticate, get_user_model
+
 # imports from local machine
 from stocks import models
 
+
 class RegisterForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class' : 'input'}))
-    email = forms.CharField(widget=forms.TextInput(attrs={'class' : 'input'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'input'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": "input"}))
+    email = forms.CharField(widget=forms.TextInput(attrs={"class": "input"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "input"}))
 
     def clean_username(self):
-        data = self.cleaned_data['username']
+        data = self.cleaned_data["username"]
         # check if this username exists already
         try:
             user_check = get_user_model().objects.get(username=data)
             # if an exception is not raised, then this user already exists
-            raise ValidationError("Your username already exists. Please choose another.")
+            raise ValidationError(
+                "Your username already exists. Please choose another."
+            )
         except models.User.DoesNotExist:
-            # continue since this user does not already exist. 
+            # continue since this user does not already exist.
             pass
         return data
 
     def clean_email(self):
-        data = self.cleaned_data['email']
+        data = self.cleaned_data["email"]
         # check if this email exists already
         try:
             email_check = get_user_model().objects.get(email=data)
             # if an exception is not raised, then this user already exists
-            raise ValidationError("This email address has already been used. Please choose another or reset the password for this user.")
+            raise ValidationError(
+                "This email address has already been used. Please choose another or reset the password for this user."
+            )
         except models.User.DoesNotExist:
-            # continue since this user does not already exist. 
+            # continue since this user does not already exist.
             pass
         # check if this is a valid email address
         validators.validate_email(data)
@@ -41,19 +47,19 @@ class RegisterForm(forms.Form):
         return data
 
     def clean_password(self):
-        data = self.cleaned_data['password']
+        data = self.cleaned_data["password"]
         # check if this password is good
         password_validators.validate_password(data)
         return data
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class' : 'input'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'input'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": "input"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "input"}))
 
     def clean_username(self):
         try:
-            data = self.cleaned_data['username']
+            data = self.cleaned_data["username"]
             # check if this email exists
             get_user_model().objects.get(username=data)
             # if an exception is not raised, then this user exists
@@ -65,12 +71,12 @@ class LoginForm(forms.Form):
 
     def clean_password(self):
         try:
-            username = self.cleaned_data['username']
-            password = self.cleaned_data['password']
+            username = self.cleaned_data["username"]
+            password = self.cleaned_data["password"]
         except KeyError:
             raise ValidationError("Please enter both a username and a password.")
         # check if this password is correct
-        user = authenticate(username=username,password=password)
+        user = authenticate(username=username, password=password)
         if user is not None:
             pass
         else:
@@ -82,41 +88,48 @@ class PortfolioTransactionForm(forms.Form):
 
     # Set up the required data for this form
     ALL_LISTED_EQUITY_CHOICES = []
-    choices = models.ListedEquities.objects.all().order_by('symbol')
+    choices = models.ListedEquities.objects.all().order_by("symbol")
     for choice in choices:
-        ALL_LISTED_EQUITY_CHOICES.append((choice.symbol,choice.symbol))
-    symbol = forms.ChoiceField(widget=forms.Select(attrs={'class' : ''}), choices = ALL_LISTED_EQUITY_CHOICES)
-    num_shares = forms.CharField(widget=forms.TextInput(attrs={'class' : 'input'}))
-    bought_or_sold =  forms.ChoiceField(widget=forms.Select(attrs={'class' : ''}), choices = (("Bought","Bought"),("Sold","Sold")))
-    price = forms.CharField(widget=forms.TextInput(attrs={'class' : 'input'}))
+        ALL_LISTED_EQUITY_CHOICES.append((choice.symbol, choice.symbol))
+    symbol = forms.ChoiceField(
+        widget=forms.Select(attrs={"class": ""}), choices=ALL_LISTED_EQUITY_CHOICES
+    )
+    num_shares = forms.CharField(widget=forms.TextInput(attrs={"class": "input"}))
+    bought_or_sold = forms.ChoiceField(
+        widget=forms.Select(attrs={"class": ""}),
+        choices=(("Bought", "Bought"), ("Sold", "Sold")),
+    )
+    price = forms.CharField(widget=forms.TextInput(attrs={"class": "input"}))
     date = forms.DateField()
 
     def clean_num_shares(self):
-        num_shares = self.cleaned_data['num_shares']
+        num_shares = self.cleaned_data["num_shares"]
         try:
             num_shares = int(num_shares)
         except ValueError:
-            raise ValidationError("You have not entered a valid number of shares. Please recheck.")
+            raise ValidationError(
+                "You have not entered a valid number of shares. Please recheck."
+            )
         return num_shares
 
     def clean_price(self):
-        price = self.cleaned_data['price']
+        price = self.cleaned_data["price"]
         try:
             # check if this is a valid float
             price = float(price)
             # check if this is positive
             if price <= 0.0:
                 raise ValueError()
-        except (ValueError,TypeError):
+        except (ValueError, TypeError):
             raise ValidationError("You have not entered a valid price. Please recheck.")
         return price
 
 
 class PasswordResetForm(forms.Form):
-    email = forms.CharField(widget=forms.TextInput(attrs={'class' : 'input'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={"class": "input"}))
 
     def clean_email(self):
-        data = self.cleaned_data['email']
+        data = self.cleaned_data["email"]
         # check if this email exists in our db
         try:
             email_check = get_user_model().objects.get(email=data)
