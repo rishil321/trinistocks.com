2657643d4a92fd6699604120bd8d857effeef069
Rishi Latchmepersad <latchmepersad@gmail.com>
2021-04-21 13:11:34 -0400

    ewew
    Former-commit-id: 49d99fd4d056a24dd5360d1d358a0828da5a82c4 [formerly 7339f30ac2c1f95db4c10746b5e621f638889bbd] [formerly a39dc56bf1563e14ad2f70fd074c1a6b49441b1b [formerly 8cf202a768dce353f4a8a6fd80dd092e41857157]]
    Former-commit-id: 4d68bc57f43039d957f7e676403eaac591fc7dc2 [formerly 37f57d046dac8518d9f4a452cc5aded48942e231]
    Former-commit-id: 2051ed6ea09c5e72004ed795d6a15df8dce4a93d

--- trinistocks/stocks/serializers.py
+++ trinistocks/stocks/serializers.py
@@ -84,7 +84,7 @@ class ListedStockSerializer(serializers.ModelSerializer):
 
 
 class FundamentalAnalysisSerializer(serializers.ModelSerializer):
-    sector = serializers.CharField(read_only=True)
+    sector = serializers.CharField(read_only=True, source="listedequities.sector")
 
     class Meta:
         model = FundamentalAnalysisSummary
