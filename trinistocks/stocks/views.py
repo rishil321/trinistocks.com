f77ebab50fdea7aea3fb954ac9f0b6f0718d22fc
Rishi Latchmepersad <latchmepersad@gmail.com>
2021-05-28 19:00:54 -0400

    added username to response
    Former-commit-id: 9872358588d374771dfc2f3711544400313aec86 [formerly 8390d5125db4cae4e4f5afe82c355182200d58b5] [formerly 9a9fc4046223969e69f4a5167ea77833f2538b1e [formerly 55e75590227efd1b73947a5ba526979871411306]]
    Former-commit-id: 5fa8149a3fb8c26c28032ed7ac6fbd9a0571dd76 [formerly b6608c5a8544ec1c4e5b43c8445aa2e281bc5dda]
    Former-commit-id: b5234ad39910201a9f153aa7c23bdbeb92ead61d

--- trinistocks/stocks/views.py
+++ trinistocks/stocks/views.py
@@ -2015,7 +2015,14 @@ class CustomAuthToken(ObtainAuthToken):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data["user"]
         token, created = Token.objects.get_or_create(user=user)
-        return Response({"token": token.key, "user_id": user.pk, "email": user.email})
+        return Response(
+            {
+                "token": token.key,
+                "user_id": user.pk,
+                "email": user.email,
+                "username": user.username,
+            }
+        )
 
 
 class ChangePasswordView(generics.UpdateAPIView):
