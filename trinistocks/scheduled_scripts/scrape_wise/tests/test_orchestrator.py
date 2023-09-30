0095e777a7a6c6070c10cc288411bbed5d1f5015
Dana Dookie <latchmepersad@gmail.com>
2023-09-30 19:20:49 -0400

    fixing script

--- trinistocks/scheduled_scripts/scrape_wise/tests/test_orchestrator.py
+++ trinistocks/scheduled_scripts/scrape_wise/tests/test_orchestrator.py
@@ -4,3 +4,8 @@ from scheduled_scripts.scrape_wise import orchestrator
 def test_scrape_and_parse_all_missing_reports():
     result: bool = orchestrator.scrape_and_parse_all_missing_reports()
     assert result is True
+
+
+def test_scrape_and_parse_specific_report():
+    result: bool = orchestrator.scrape_and_parse_specific_report("27/09/2023")
+    assert result is True
